dc31aee1e6f4b9934f6a6bc60e27517337ffcdf9
Edgar <edgar.h.salinas@gmail.com>
2018-08-02 21:41:19 -0700

    Imported angular material for form styling

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,18 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   enteredValue = '';
   newPost = 'NONE';
 
-  onAddPost(postInput: HTMLTextAreaElement) {
-    console.dir(postInput);
-    this.newPost = postInput.value;
-  }
-
-  onAddPost2way() {
-    this.newPost2way = this.enteredValue;
+  onAddPost() {
+    this.newPost = this.enteredValue;
   }
 }
