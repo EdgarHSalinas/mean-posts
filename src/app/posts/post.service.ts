4483dfb5d61edaf4df1cb65320be76cd9a550a94
Edgar <edgar.h.salinas@gmail.com>
2018-08-07 20:00:34 -0700

    Implemented Backend Routing with Router Express and FrontEnd Routing Module

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '../../../node_modules/@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Router } from '../../../node_modules/@angular/router';
 
 import { Post } from './post.model';
-import { post } from '../../../node_modules/@types/selenium-webdriver/http';
 
 @Injectable({providedIn: 'root'})
 export class PostService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http
@@ -38,6 +37,12 @@ export class PostService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      "http://localhost:3000/api/posts/" + id
+    );
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content};
     this.http
@@ -47,8 +52,25 @@ export class PostService {
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
       });
   }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        console.log(response)
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+        this.router.navigate(['/']);
+      });
+  }
+
 
   deletePost(postId: string) {
     this.http.delete('http://localhost:3000/api/posts/' + postId)
